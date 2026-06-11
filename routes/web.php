dd305878a41f434b6f5b0cd5de22dafac4d254b7
vshndrr-code <vshndrr@gmail.com>
2026-06-11 19:46:50 +0700

    misaih dashboard

--- routes/web.php
+++ routes/web.php
@@ -97,6 +97,7 @@ Route::middleware('auth')->group(function () {
 
 Route::middleware(['auth', 'verified', 'auditor'])->prefix('auditor')->name('auditor.')->group(function () {
     Route::get('/dashboard', [AuditorController::class, 'dashboard'])->name('dashboard');
+    Route::get('/categories', [AuditorController::class, 'categories'])->name('categories.index');
     Route::post('/categories', [AuditorController::class, 'storeCategory'])->name('categories.store');
     Route::get('/tags', [AuditorController::class, 'tags'])->name('tags.index');
     Route::delete('/tags/{tag}', [AuditorController::class, 'destroyTag'])->name('tags.destroy');
